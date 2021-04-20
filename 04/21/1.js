7df3c49f61e5a914a9d127b92c34eca534d6b788
JinXiaoJian <jinxiaojian@threatbook.cn>
2021-04-20 15:02:19 +0800

    fix

--- 04/21/1.js
+++ 04/21/1.js
@@ -1,46 +1,47 @@
 /*
- * @LastEditTime: 2021-04-20 14:26:21
+ * @LastEditTime: 2021-04-20 15:02:00
  * @LastEditors: jinxiaojian
  */
-function TestFunctionState() {
+function TestFunctionState () {
   const [count, setCount] = React.useState(0);
   const btnRef = React.useRef(null);
-  const eventRef=React.useRef(null);
-  eventRef.current={onClick};
+  const eventRef = React.useRef(null);
+  eventRef.current = { onClick };
   React.useEffect(() => {
-    function onProxyClick(e){
+    function onProxyClick (e) {
       eventRef.current.onClick(e);
-      console.log('proxy');
     }
+    // 无法得到准确值
     btnRef.current.addEventListener("click", onClick);
+    // 通过转发获取准确值
     btnRef.current.addEventListener("click", onProxyClick);
   }, []);
-  function onClick() {
+  function onClick () {
     console.log(count);
   }
-  function handleClick(){
-    setCount(count+1)
+  function handleClick () {
+    setCount(count + 1)
   }
   React.useEffect(() => {
     setCount(777);
   }, []);
   return (
     <div>
-      <p>useState api</p>
+      <p>获取 useState conunt 值</p>
       <p>
         <button ref={btnRef}>打印count</button>
-        Count: {count} <button onClick={handleClick}>自增</button>
+        Count: {count} <button onClick={handleClick}>加一</button>
       </p>
     </div>
   );
 }
 
 class App extends React.Component {
-  
-  render() {
+
+  render () {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>addEventListener</h1>
         <h2>Start editing to see some magic happen!</h2>
         <TestFunctionState />
       </div>
